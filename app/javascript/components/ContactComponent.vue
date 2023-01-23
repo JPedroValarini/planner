f52f599dd282bc8527c603cd27b77e3cf0e95f26
joao <joao@moat.ai>
2023-01-23 17:02:59 -0300

    vue configuration

---
+++ app/javascript/components/ContactComponent.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div id="app">
+    
+  </div>
+</template>
+<script>
+export default {
+  props: [ 'contact' ],
+  data(){
+    return {
+      testando: '',
+    };
+  },
+  mounted() {
+    console.log(this.contact)
+  }
+}
+</script>
+<style lang="">
+  
+</style>
