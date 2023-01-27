c18e6ddb25af5d7ac86b90fb4ff5802ac63276a6
joao <joao@moat.ai>
2023-01-26 22:29:51 -0300

    layout changes some config

--- app/javascript/components/ContactComponent.vue
+++ app/javascript/components/ContactComponent.vue
@@ -1,21 +1,47 @@
 <template lang="">
   <div id="app">
-    
+    <h1>Contacts</h1>
+    <div v-if="loading">
+      <table class="table container">
+        <thead class="table-dark">
+          <tr>
+            <th>Full name</th>
+            <th>Cpf</th>
+            <th>Email</th>
+            <th>Birthdate</th>
+            <th>Phone</th>
+            <th colspan="3"></th>
+          </tr>
+        </thead>
+        <tbody v-for="contato in contatos">
+          <tr>
+            <td>{{ contato.full_name }}</td>
+            <td>{{ contato.cpf }}</td>
+            <td>{{ contato.email }}</td>
+            <td>{{ contato.birthdate }}</td>
+            <td>{{ contato.phone }}</td>
+            <a href="" class="btn-show"><i class="bi bi-arrow-right-short"></i></i></a>
+          </tr>
+        </tbody>
+      </table>
+      <br />
+    </div>
   </div>
 </template>
 <script>
 export default {
-  props: [ 'contact' ],
-  data(){
+  props: ["contact"],
+  data() {
     return {
-      testando: '',
+      contatos: {},
+      loading: false,
     };
   },
-  mounted() {
-    console.log(this.contact)
-  }
-}
+  beforeMount() {
+    this.contatos = JSON.parse(this.contact);
+    this.loading = true;
+  },
+};
 </script>
 <style lang="">
-  
 </style>
